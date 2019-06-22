d490b95d2429039f53d5c620abd3dd74f831cd45
zkhalido <46911135+zkhalido@users.noreply.github.com>
2019-06-22 00:01:22 -0600

    winner checker working, altered main to run till there is a whinner

--- tic_tac_toe_AI.py
+++ tic_tac_toe_AI.py
@@ -2,15 +2,17 @@ import tictactoe
 
 if '__main__' == __name__:
     turn = 0
+    winner = False
     board = tictactoe.Board.new_board()
+
+    while winner == False:
+        tictactoe.Render.print_render(board)
+        move_x, move_y = tictactoe.GetMove.get_move()
+        current_player = tictactoe.SwitchPlayer.switch_player(turn)
+        tictactoe.MakeMove.make_move(board, move_x, move_y, current_player)
+        turn += 1
+        winner = tictactoe.GetWinner.get_winner(board)
+
     tictactoe.Render.print_render(board)
-    move_x, move_y = tictactoe.GetMove.get_move()
-    current_player = tictactoe.SwitchPlayer.switch_player(turn)
-    tictactoe.MakeMove.make_move(board, move_x, move_y, current_player)
-    turn += 1
-    tictactoe.Render.print_render(board)
-    move_x, move_y = tictactoe.GetMove.get_move()
-    current_player = tictactoe.SwitchPlayer.switch_player(turn)
-    tictactoe.MakeMove.make_move(board, move_x, move_y, current_player)
-    turn += 1
-    tictactoe.Render.print_render(board)
+    if winner == True:
+        print("\nWinner")
--- tictactoe/__init__.py
+++ tictactoe/__init__.py
@@ -3,3 +3,4 @@ from .render import Render
 from .get_move import GetMove
 from .switch_player import SwitchPlayer
 from .make_move import MakeMove
+from .get_winner import GetWinner
